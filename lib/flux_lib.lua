04ef9ca3140603d5ce425f40410ef4bb4c42a309
Alexei Osinin <osinin.dev@gmail.com>
2024-09-14 20:09:20 +0700

    Improved energy measurement

--- lib/flux_lib.lua
+++ lib/flux_lib.lua
@@ -1,50 +1,32 @@
 local fluxLib = {}
 local component = require "component"
-local flux = component.flux_controller
+local flux = component.flux_storage
+local prefixes = { "k", "M", "G" }
 
 function fluxLib.getInputEnergy()
-    local arr = {}
-    for i = 1, 50 do
-        table.insert(arr, flux.getEnergyInfo().energyInput)
-        os.sleep(0.05)
-    end
-    local freq = {}
-    for i = 1, #arr do
-        if freq[arr[i]] == nil then
-            freq[arr[i]] = 1
-        else
-            freq[arr[i]] = freq[arr[i]] + 1
+    return flux.getEnergyInfo().energyInput
+end
+
+function fluxLib.format(rf, precision)
+    local prefix = ""
+    for i = 1, #prefixes do
+        if (rf < 1024) then
+            break
         end
+        prefix = prefixes[i];
+        rf = rf / 1000;
     end
-    local max_freq = 0
-    local max_val = nil
-    for k, v in pairs(freq) do
-        if v > max_freq then
-            max_freq = v
-            max_val = k
-        end
-    end
-    return max_val
+    local formattedRf = string.format("%." .. precision .. "f", rf)
+        :gsub("%.?0+$", "")
+    return formattedRf .. " " .. prefix .. "Rf/t"
 end
 
 function fluxLib.formatDouble(rf)
-    if (rf <= 0 or rf / 1000 <= 1) then
-        return string.format("%.3f", tostring(rf)) .. " Rf/t"
-    elseif rf / 1000000 >= 1 then
-        return string.format("%.3f", tostring(rf / 1000000)) .. " MRf/t"
-    else
-        return string.format("%.1f", tostring(rf / 1000)) .. " kRf/t"
-    end
+    return fluxLib.format(rf, 3)
 end
 
 function fluxLib.formatInt(rf)
-    if (rf <= 0 or rf / 1000 <= 1) then
-        return string.format("%d", math.floor(rf)) .. " Rf/t"
-    elseif rf / 1000000 >= 1 then
-        return string.format("%d", math.floor(rf / 1000000)) .. " MRf/t"
-    else
-        return string.format("%d", math.floor(rf / 1000)) .. " kRf/t"
-    end
+    return fluxLib.format(rf, 0)
 end
 
 function fluxLib.getName()
