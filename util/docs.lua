99e9e31aba47c138021ca8be54f688c71746a981
Alexei Osinin <osinin.dev@gmail.com>
2026-01-24 21:01:59 +0700

    Added utility for documenting component methods and uploading to Pastebin

---
+++ util/docs.lua
@@ -0,0 +1,50 @@
+local component = require("component")
+local shell = require("shell")
+local filesystem = require("filesystem")
+local term = require("term")
+
+local function getMethodsDocs(address, typeName)
+  local lines = { "Component: " .. typeName, "Address: " .. address, "" }
+  local methods = component.methods(address)
+  for name, _ in pairs(methods) do
+    local docs = component.doc(address, name)
+    table.insert(lines, ("Method: %s\nDocs: %s\n"):format(name, docs or "N/A"))
+  end
+  return table.concat(lines, "\n")
+end
+
+local function uploadToPastebin(content, filename)
+  local tmpPath = "/tmp/" .. filename
+  local file = io.open(tmpPath, "w")
+  file:write(content)
+  file:close()
+
+  shell.execute("pastebin put " .. tmpPath)
+
+  filesystem.remove(tmpPath)
+end
+
+local typeName = ...
+
+if not typeName or typeName == "" then
+  io.stderr:write("Usage: docs <componentType>\n")
+  return
+end
+
+-- Берём первый попавшийся компонент указанного типа (точное совпадение).
+local address = component.list(typeName, true)()
+
+if not address then
+  io.stderr:write(("Component type '%s' not found.\n"):format(typeName))
+  return
+end
+
+local docs = getMethodsDocs(address, typeName)
+
+print(docs)
+io.write("\nUpload to Pastebin? [y/n]: ")
+local input = term.read()
+input = input and input:gsub("%s+", "") or ""
+if input:lower() == "y" then
+  uploadToPastebin(docs, typeName .. "_methods.txt")
+end
